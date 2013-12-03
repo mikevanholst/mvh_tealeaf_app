8e10e734269726f42590f26944b8ef1a13fa979b
Michael van Holst <mikevanholst@gmail.com>
2013-12-03 18:39:01 -0500

    adds a collection and a member route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 MvhTealeafApp::Application.routes.draw do
-  resources :users
+  resources :users do
+    collection do
+      get 'search', to: 'users#search'      
+    end
+
+    member do
+      post 'highlight', to: 'users#highlight'
+    end
+  end
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
