8b1bc65077e4e67faebeeb2c0cd9cddb4fecf593
Michael van Holst <mikevanholst@gmail.com>
2013-12-03 13:20:27 -0500

    adds rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MvhTealeafApp::Application.config.secret_key_base = '01b93a039ec83a82d51833c5d2cd59b4f9c2855de2c96d234f9bccdf306b7566273c370bf2cb5198ba4b3194c5a41fe568e0ed5464c97434b8542650e98afc4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MvhTealeafApp::Application.config.session_store :cookie_store, key: '_mvh_tealeaf_app_session'
